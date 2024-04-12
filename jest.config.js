03a33eb022a11ec36ff59858d85d35097c19fe2d
William Koller <williamkoller30@gmail.com>
2024-04-12 17:13:11 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main.ts',
+    '!<rootDir>/src/app.module.ts',
+    '!<rootDir>/src/core/**',
+    '!<rootDir>/src/data/**',
+    '!<rootDir>/src/infrastructure/database/database.module.ts',
+    '!<rootDir>/src/infrastructure/database/prisma/config/**',
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  testRegex: '.*\\.spec\\.ts$',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75,
+    },
+  },
+};
